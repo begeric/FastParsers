f88fa4fb4e697ffc2606e98ebcc4f78e84e557f8
begeric <begueteric@gmail.com>
2014-05-10 08:50:52 +0200

    Add example of generated code

--- FastParsers/src/main/scala/fastparsers/framework/getAST.scala
+++ FastParsers/src/main/scala/fastparsers/framework/getAST.scala
@@ -7,14 +7,14 @@ import scala.reflect.macros.whitebox.Context
  * Created by Eric on 22.04.14.
  */
 object getAST {
-  def get(parser: Any):Any = macro getImpl
+  def get(code: Any):Any = macro getImpl
 
-  def getImpl(c: Context)(parser: c.Tree): c.Tree = {
+  def getImpl(c: Context)(code: c.Tree): c.Tree = {
     import c.universe._
 
-    val tmp = q"$parser"
+    val tmp = q"$code"
     val elem: c.Type = c.typecheck(tq"Char",c.TYPEmode).tpe
     val input: c.Type = typeOf[Array[Char]]
-    c.abort(c.enclosingPosition,show(parser.tpe))
+    c.abort(c.enclosingPosition,show(code))
   }
 }
--- FastParsers/src/main/scala/fastparsers/framework/ruleprocessing/FinalFastParserImpl.scala
+++ FastParsers/src/main/scala/fastparsers/framework/ruleprocessing/FinalFastParserImpl.scala
@@ -3,7 +3,7 @@ package fastparsers.framework.ruleprocessing
 /**
  * Created by Eric on 22.04.14.
  * Trait used to recognise other parsers.
- * val parser = FastParser {...}
- * The type of parser will be AnyRef extends FinalFastParserImpl {..}
+ * val code = FastParser {...}
+ * The type of code will be AnyRef extends FinalFastParserImpl {..}
  */
 trait FinalFastParserImpl
