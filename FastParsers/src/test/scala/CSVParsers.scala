80c3fe1874208f2527c22671771353a18b748b15
begeric <begueteric@gmail.com>
2014-05-15 22:20:56 +0200

    Forgot to add CSVParsers

---
+++ FastParsers/src/test/scala/CSVParsers.scala
@@ -0,0 +1,48 @@
+import fastparsers.input.InputWindow
+import fastparsers.parsers.Parser
+import scala.util.parsing.combinator._
+import scala.util.parsing.input._
+
+import fastparsers.framework.getAST
+
+/**
+ * Created by Eric on 05.04.14.
+ */
+object CSVParsers {
+  
+    import fastparsers.framework.implementations.FastParsersCharArray._
+    //GROS HACK
+    import fastparsers.input.InputWindow.InputWindow
+
+    sealed abstract class JSValue
+    case class JSArray(arr: List[JSValue]) extends JSValue
+    case class JSDouble(d: Double) extends JSValue
+    case class JSBool(b: Boolean) extends JSValue
+    object JTrue extends JSValue
+    object JFalse extends JSValue
+
+    val trueValue = "true".toCharArray
+    val falseValue = "false".toCharArray
+    val comma = ",".toCharArray
+    val close = "]".toCharArray
+
+    /*val cvsParser = FastParsersCharArray  {
+		def primDouble = decimalNumber ^^ (y => JSDouble(y.toString.toDouble))
+		def doubles = '[' ~> repsep(primDouble, comma) <~ close ^^ (x => JSArray(x))
+		
+		def primBools = (lit(trueValue) ~> success(JTrue)) | (lit(falseValue) ~> success(JFalse)) 
+		def bools = '[' ~> repsep(primBools, comma) <~ close ^^ (x => JSArray(x))
+	}*/
+	
+	val cvsParserOpt = (FastParsersCharArray  {
+		def primBools = ('t' ~ 'r' ~ 'u' ~ 'e' ~> success(JTrue)) | ('f' ~ 'a' ~ 'l' ~'s' ~ 'e' ~> success(JFalse)) 
+		def bools = '[' ~> repsep(primBools, ',') <~ close// ^^ (x => JSArray(x))
+	})
+	
+	val cvsParser = FastParsersCharArray  {
+		def cvs(p: Parser[JSValue]) = '[' ~> repsep(p, comma) <~ close ^^ (x => JSArray(x))
+		def primDouble = cvs(decimalNumber ^^ (y => JSDouble(y.toString.toDouble)))
+		def bools = cvs((lit(trueValue) ~> success(JTrue)) | (lit(falseValue) ~> success(JFalse))) 
+	}
+
+}
