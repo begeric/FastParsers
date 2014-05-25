290a84558694e1f36eb015daa6d5dc44d1ade3d9
begeric <begueteric@gmail.com>
2014-05-25 18:41:39 +0200

    forgot to add FinalFastParserImpl.scala file

---
+++ FastParsers/src/main/scala/fastparsers/framework/implementations/FinalFastParserImpl.scala
@@ -0,0 +1,9 @@
+package fastparsers.framework.implementations
+
+/**
+ * Created by Eric on 22.04.14.
+ * Trait used to recognise other parsers.
+ * val parser = FastParser {...}
+ * The type of parser will be AnyRef extends FinalFastParserImpl {..}
+ */
+trait FinalFastParserImpl
