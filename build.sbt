dc24d6c9c28b710fa18bd935859c1b36e84c396d
begeric <begueteric@gmail.com>
2014-09-12 15:01:11 +0200

    Update Libraries versions

--- build.sbt
+++
@@ -1,9 +0,0 @@
-name := "FastParsers"
-
-scalaVersion := "2.11.0-RC4"
-
-scalacOptions ++= Seq(
-  "-optimize"
-)
-
-libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value // - See more at: http://typesafe.com/blog/what-happened-to-my-travis-ci#
--- project/Build.scala
+++ project/Build.scala
@@ -1,39 +1,48 @@
 import sbt._
 import Keys._
 
-object MacroBuild extends Build {
+object FastParsersBuild extends Build {
 
-   lazy val Examples = project.in(file("Examples")) dependsOn(FastParsers) settings(
+   def commonSettings = Defaults.coreDefaultSettings ++ Seq(
+	   scalaVersion := "2.11.2" ,
+       scalacOptions := Seq("-optimize"),
+	   libraryDependencies ++=  Seq(
+		"org.scala-lang" % "scala-compiler"  % scalaVersion.value % "provided",
+		"org.scala-lang" % "scala-reflect" % scalaVersion.value 
+	)
+   )
+
+
+   lazy val Examples = Project(
+	id = "Examples",
+	base = file("Examples"),	
+	dependencies = Seq(FastParsers),
+	settings = commonSettings ++ Seq (
 	   // include the macro classes and resources in the main jar
-	   scalaVersion := "2.11.0-RC4" ,
-	   	
 	   mappings in (Compile, packageBin) ++= mappings.in(FastParsers, Compile, packageBin).value,
 	   // include the macro sources in the main source jar
 	   mappings in (Compile, packageSrc) ++= mappings.in(FastParsers, Compile, packageSrc).value
 	)
+   )
 	
-   lazy val FastParsers = project.in(file("FastParsers")) settings(
-		scalaVersion := "2.11.0-RC4" ,
-		
+   lazy val FastParsers = Project(
+	id = "FastParsers",
+	base = file("FastParsers"),	
+	settings = commonSettings ++ Seq (
 		resolvers ++= Seq(
-		  "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases",
-		  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"/*,
-		  Classpaths.sbtPluginReleases*/
+		  "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"
 		),
 		//addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.98.0"),
 			
-	   libraryDependencies += "org.scalatest"  % "scalatest_2.10"   % "1.9.1" % "test",
+		libraryDependencies += "org.scalatest"  % "scalatest_2.11"   % "2.2.1" % "test",
 		
-		libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.3" % "test",
+		libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
 		
-		libraryDependencies += "com.github.axel22" % "scalameter_2.10" % "0.5-SNAPSHOT" % "test",
+		libraryDependencies += "com.storm-enroute" % "scalameter_2.11" % "0.6" % "test",
 
-		libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.11.0-RC4",
-		
-		libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-RC4",
-		
 		testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-		
-		scalacOptions ++= Seq("-optimize")
+  
+    	logBuffered := false
+	)
    )
 }
